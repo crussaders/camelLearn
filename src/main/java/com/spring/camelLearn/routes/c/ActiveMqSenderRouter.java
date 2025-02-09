b81cc10775cb5e320108da8e3abbc76fcdab0cf6
manishpandey <pandeym321@gmail.com>
2025-02-09 17:19:47 +0530

    kafka and activemq changes

--- src/main/java/com/spring/camelLearn/routes/c/ActiveMqSenderRouter.java
+++ src/main/java/com/spring/camelLearn/routes/c/ActiveMqSenderRouter.java
@@ -8,10 +8,19 @@ public class ActiveMqSenderRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         //TIMER
-        from("timer:active-mq-timer?period=10000")
-                .transform().constant("My message for ActiveMq")
-                .log("${body}")
-                .to("activemq:my-activemq-queue");
+//        from("timer:active-mq-timer?period=10000")
+//                .transform().constant("My message for ActiveMq")
+//                .log("${body}")
+//                .to("activemq:my-activemq-queue");
         //QUEUE
+
+//        LISTENING FROM FILE
+//                from("file:files/json")
+//                .log("${body}")
+//                .to("activemq:my-activemq-queue");
+        from("file:files/xml")
+                .log("${body}")
+                .to("activemq:my-activemq-xml-queue");
     }
-}
+    }
+
